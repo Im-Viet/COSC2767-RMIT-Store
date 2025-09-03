3d1a117a6374557afde3e420932f934840fb8d29
Viet Anh <s3988393@rmit.edu.vn>
2025-09-03 15:24:03 +0700

    make changes

--- client/docker-entrypoint.sh
+++ client/docker-entrypoint.sh
@@ -1,15 +1,10 @@
 #!/bin/sh
 set -e
 
-: "${API_URL:=http://backend-svc:3000/api}"
+: "${API_URL:=/api}"          # ⬅️ CHANGED default from http://backend-svc:3000/api to /api
 : "${HOST:=0.0.0.0}"
 : "${PORT:=8080}"
 
 echo "Using API_URL=${API_URL}"
-echo "Dev server binding on ${HOST}:${PORT}"
-
-# Write .env for dotenv-webpack to pick up at startup
 printf "API_URL=%s\n" "$API_URL" > .env
-
-# Start webpack-dev-server with proper host/port
 npm run dev -- --host "${HOST}" --port "${PORT}"
