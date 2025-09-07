43a59dd13b02577148b8152fc750b0813d5e5a65
Viet Anh <s3988393@rmit.edu.vn>
2025-09-07 08:53:06 +0700

    test login

--- server/tests/integration/auth.login.test.js
+++ server/tests/integration/auth.login.test.js
@@ -3,22 +3,24 @@ const bcrypt = require('bcryptjs');
 const buildTestApp = require('../helpers/buildTestApp');
 const User = require('../../models/user');
 const { EMAIL_PROVIDER } = require('../../constants');
+const { ROLES } = require('../../constants');
 
 const app = buildTestApp();
 
-describe('POST /api/auth/login', () => {
+describe('POST /api/login', () => {
   const email = 'admin@example.com';
   const password = 'P@ssw0rd!';
 
   beforeEach(async () => {
-    const hash = await bcrypt.hash(password, 10);
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
     await User.create({
       email,
       password: hash,
       provider: EMAIL_PROVIDER.Email,
-      role: 'ROLE ADMIN',
-      firstName: 'Admin',
-      lastName: 'User'
+      role: ROLES.USER,
+      firstName: 'user',
+      lastName: 'user'
     });
   });
 
